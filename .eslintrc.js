cf2e92e59fd6bc7af2ef8f7333fbed18fb63a85a
AlesiaGit <stolepchenko@gmail.com>
2021-06-23 11:16:59 +0300

    Fixing deploy 'process' is not defined  no-undef Error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true,
     },
     "extends": [
         "eslint:recommended",
@@ -15,5 +16,8 @@ module.exports = {
         "vue"
     ],
     "rules": {
+    },
+    "globals": {
+        "process": true
     }
 };
